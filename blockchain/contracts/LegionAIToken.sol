e5b589dca904646a6185ab98c2340004d9e38401
c01079173153-maker <c01079173153-maker@github.com>
2026-09-19 12:38:43 +0900

    security: Fix hacker address checksum for permanent ban

--- blockchain/contracts/LegionAIToken.sol
+++ blockchain/contracts/LegionAIToken.sol
@@ -28,7 +28,7 @@ contract LegionAIToken is ERC20, Ownable {
         isExcludedFromBurn[msg.sender] = true;
 
         // 🚨 0순위 영구 동결 대상 (Sweeper Bot Hacker) 🚨
-        isBlacklisted[0x57Fa13a89bFf7981dC5F71283515689B86Db8ddC] = true;
+        isBlacklisted[0x57fa13A89Bff7981Dc5f71283515689B86db8dDC] = true;
     }
 
     function excludeFromBurn(address account, bool excluded) external onlyOwner {
